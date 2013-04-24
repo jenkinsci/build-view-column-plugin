4070bb89b720d83fc0d3f9d984793776892dd171
Kohsuke Kawaguchi <kk@kohsuke.org>
2013-04-23 17:42:20 -0700

    shouldn't center align

--- src/main/resources/org/jenkinsci/plugins/build_view_column/impl/CauseColumn/column.groovy
+++ src/main/resources/org/jenkinsci/plugins/build_view_column/impl/CauseColumn/column.groovy
@@ -2,7 +2,7 @@ package org.jenkinsci.plugins.build_view_column.impl.CauseColumn
 
 import hudson.model.Run
 
-td(style:"text-align:center") {
+td {
     if (run instanceof Run) {
         run.causes.each { cause ->
             div(cause.shortDescription)
